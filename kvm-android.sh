45b1a4767bc177084758f0420fecdc54fc3bee22
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-09-26 22:27:20 +0200

    create bigger boot image for froyo

--- kvm-android.sh
+++ kvm-android.sh
@@ -4,13 +4,16 @@
 # http://www.android-x86.org/documents/qemuhowto
 #qemu-kvm -soundhw es1370 -net nic -net user -cdrom vm.iso
 
+#name=android-x86-1.6-r2
+size=300M
+
 name=froyo
-name=android-x86-1.6-r2
+size=600M
 
 boot=emulator/$name.boot.qcow2
 sdcard=emulator/$name.sdcard.qcow2
 
-test -f $boot   || qemu-img create $boot   512M && cdrom="-cdrom emulator/$name.iso"
+test -f $boot   || qemu-img create $boot   $size && cdrom="-cdrom emulator/$name.iso"
 test -f $sdcard || qemu-img create $sdcard 256M
 
 kvm -redir tcp:5555::5555 -soundhw ac97 -m 256 -net nic -net user -hda $boot -hdb $sdcard $cdrom
