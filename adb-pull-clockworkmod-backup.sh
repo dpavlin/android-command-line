202399c196c8ab5b60146e67b717ad668f03e29d
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-10-01 19:04:10 +0200

    pull backup incrementaly
    it's more or less a mess of ls, diff and xargs to pull just new
    files using adb

--- adb-pull-clockworkmod-backup.sh
+++ adb-pull-clockworkmod-backup.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -x
 
-adb pull /sdcard/clockworkmod/backup backup
+sdcard=/sdcard/clockworkmod/
+
+adb shell ls -1 -d $sdcard/backup/* | sed "s!$sdcard/*!!" | tr -d '\r' > /tmp/backup.android
+ls -1 -d backup/* > /tmp/backup.disk
+diff -uw /tmp/backup.android /tmp/backup.disk | grep -- '^-backup' | sed 's/^-//' | \
+xargs -i sh -xc "mkdir {} && adb pull $sdcard/{} {}"
