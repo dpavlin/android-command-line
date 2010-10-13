54e0befe9e3913a3567e3188d33d7d72a1bbb66f
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-10-13 17:43:26 +0200

    experiment with generic_x86 froyo, doesn't work

--- kvm-android.sh
+++ kvm-android.sh
@@ -4,11 +4,18 @@
 # http://www.android-x86.org/documents/qemuhowto
 #qemu-kvm -soundhw es1370 -net nic -net user -cdrom vm.iso
 
+soundhw=es1370
+soundhw=ac97
+#soundhw=sb16
+kvm="-soundhw $soundhw"
+
 #name=android-x86-1.6-r2
 size=300M
 
 name=froyo
 size=600M
+
+name=generic_x86
 
 boot=emulator/$name.boot.qcow2
 sdcard=emulator/$name.sdcard.qcow2
@@ -16,4 +23,6 @@ sdcard=emulator/$name.sdcard.qcow2
 test -f $boot   || qemu-img create $boot   $size && cdrom="-cdrom emulator/$name.iso"
 test -f $sdcard || qemu-img create $sdcard 256M
 
-kvm -redir tcp:5555::5555 -soundhw ac97 -m 256 -net nic -net user -hda $boot -hdb $sdcard $cdrom
+kvm="$kvm -monitor stdio"
+
+kvm $kvm -redir tcp:5555::5555 -m 256 -net nic -net user -hda $boot -hdb $sdcard $cdrom
