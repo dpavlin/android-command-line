271f2eb44d163af24118a97f7d7d5c0dcaf6fd73
Dobrica Pavlinusic <dpavlin@rot13.org>
2014-07-08 13:54:47 +0200

    incremental pull of new media files

--- adb-dcim.sh
+++ adb-dcim.sh
@@ -1,4 +1,10 @@
 #!/bin/sh -x
 
-mkdir /tmp/DCIM
-adb pull /sdcard/DCIM /tmp/DCIM/
+sdcard=/sdcard/DCIM/Camera/
+to=/data/DCIM/Camera/
+
+adb shell find $sdcard -mindepth 1 -maxdepth 1 | sed "s!$sdcard/*!!" | tr -d '\r' | sort > /tmp/dcim.android
+test -d $to || mkdir $to
+find $to -mindepth 1 -maxdepth 1 | sed "s!$to!!" | sort > /tmp/dcim.disk
+diff -uw /tmp/dcim.android /tmp/dcim.disk | grep -- "^-" | sed 's/^-//' | \
+xargs -i sh -xc "adb pull $sdcard/{} $to/{}"
