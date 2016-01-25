72ec097e130010eff82aa454fa896b1fb2ee7be3
unknown <bschaum@bschaum.NavNGo.local>
2016-01-25 08:39:58 +0100

    remove '\r'-s, add adb shell command to pull wildcard file, watch the '()' containing filenames

--- adb-pull-wildcard.sh
+++ adb-pull-wildcard.sh
@@ -2,5 +2,5 @@
 
 test -z "$1" && echo "usage: $0 /sdcard/DCIM/Camera/*20121111*" && exit 1
 
-ls $1 | tr -d '\r' | xargs -i sh -c 'echo {} ; adb pull {} .'
+adb shell ls $1 | tr -d '\r' | xargs -i sh -c 'echo "{}" ; adb pull "{}" .'
 
