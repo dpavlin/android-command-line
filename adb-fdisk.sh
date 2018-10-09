5e5ef5eaa0772b31f0436e4f8d55d9df3b250e3d
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-10-09 18:42:43 +0200

    added PARTNAME from uevent

--- adb-fdisk.sh
+++ adb-fdisk.sh
@@ -4,5 +4,7 @@ block=mmcblk0
 
 adb shell grep . /sys/block/$block/*p*/start | sed -e 's,/start:, ,' -e 's/[\r\n]$//'  > /dev/shm/$block.start
 adb shell grep . /sys/block/$block/*p*/size  | sed -e 's,/size:, ,'  -e 's/[\r\n]$//'  > /dev/shm/$block.size
-join /dev/shm/mmcblk0.start /dev/shm/mmcblk0.size | sort -k 2 -n | column -t | tee /dev/shm/$block.part
+adb shell grep PARTNAME /sys/block/$block/*p*/uevent  | sed -e 's,/uevent:PARTNAME=, ,'  -e 's/[\r\n]$//'  > /dev/shm/$block.name
+join /dev/shm/$block.start /dev/shm/$block.size > /dev/shm/$block.2
+join /dev/shm/$block.2     /dev/shm/$block.name | sort -k 2 -n | column -t | tee /dev/shm/$block.part
 
